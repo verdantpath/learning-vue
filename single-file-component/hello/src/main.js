6973fa221cbb967df22c9a5ea0d7ba36de0b3686
verdantpath <m_dittmann@yahoo.com>
2019-09-19 19:31:49 -0700

    add main.js

--- single-file-component/hello/src/main.js
+++ single-file-component/hello/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
+import HelloComponent from './HelloComponent.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  components: { HelloComponent }
+});
