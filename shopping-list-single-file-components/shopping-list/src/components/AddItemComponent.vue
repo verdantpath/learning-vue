4db0a93496deed6799bf751c3403bf86001ef945
verdantpath <m_dittmann@yahoo.com>
2019-09-23 21:53:21 -0700

    add component files

---
+++ shopping-list-single-file-components/shopping-list/src/components/AddItemComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    add items
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ shopping-list-single-file-components/shopping-list/src/components/ItemsComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    items
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+</style>
