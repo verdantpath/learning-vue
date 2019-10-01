198d15a547e4e72425e12c870d2bfaa378cb1a7b
verdantpath <m_dittmann@yahoo.com>
2019-09-30 21:29:43 -0700

    add addItem and changeTitle functionality

--- shopping-list-single-file-components/shopping-list/src/App.vue
+++ shopping-list-single-file-components/shopping-list/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app" class="container">
     <h2>{{ title }}</h2>
-    <add-item-component></add-item-component>
-    <items-component></items-component>
+    <add-item-component v-on:add="addItem"></add-item-component>
+    <items-component :items="items"></items-component>
     <div class="footer">
       <hr />
-      <change-title-component></change-title-component>
+      <change-title-component v-model="title"></change-title-component>
     </div>
   </div>
 </template>
@@ -25,7 +25,7 @@ export default {
     return {
       items: [{ text: 'Bananas', checked: true },
         { text: 'Apples', checked: false }],
-      title: 'Shopping List'
+      title: 'Dick Cumming List'
     }
   },
   methods: {
