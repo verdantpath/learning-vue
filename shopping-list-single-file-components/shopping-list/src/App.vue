cd01d06a7a744e0158ddc3248acb5a33d785820d
verdantpath <m_dittmann@yahoo.com>
2019-11-02 14:45:01 -0700

    bind list id to shopping list component, change to shorthand

--- shopping-list-single-file-components/shopping-list/src/App.vue
+++ shopping-list-single-file-components/shopping-list/src/App.vue
@@ -8,7 +8,7 @@
     </ul>
     <div class="tab-content">
       <div v-bind:class= "index === 0 ? 'active' : ''" v-for="(list, index) in shoppinglists" class="tab-pane" role="tabpanel" v-bind:id="list.id">
-        <shopping-list-component v-bind:title="list.title" v-bind:items="list.items"></shopping-list-component>
+        <shopping-list-component :id="list.id" :title="list.title" :items="list.items"></shopping-list-component>
       </div>
     </div>
   </div>
