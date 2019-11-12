5f0d88ca819168141d117b9d13e4c6f3785523b7
verdantpath <m_dittmann@yahoo.com>
2019-11-11 20:02:23 -0800

    add state, mutations, initialize Vuex store

--- simple-store/src/vuex/store.js
+++ simple-store/src/vuex/store.js
@@ -1,3 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
+
+const state = {
+    msg: 'Hello Vue!'
+}
+
+const mutations = {
+    changeMessage(state, msg) {
+        state.msg = msg
+    }
+}
+
+export default new Vuex.Store({
+    state: state,
+    mutations: mutations
+})
