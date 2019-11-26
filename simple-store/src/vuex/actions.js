f51b24da9bddc2f50a845a2a7df21b512b16fd58
verdantpath <m_dittmann@yahoo.com>
2019-11-25 19:01:14 -0800

    add actions

---
+++ simple-store/src/vuex/actions.js
@@ -0,0 +1,10 @@
+import { CHANGE_MSG, INCREMENT_COUNTER } from './mutation_types'
+
+export default {
+    changeMessage ({ commit }, msg) {
+        commit(CHANGE_MSG, msg)
+    },
+    incrementCounter ({ commit }) {
+        commit(INCREMENT_COUNTER)
+    }
+}
