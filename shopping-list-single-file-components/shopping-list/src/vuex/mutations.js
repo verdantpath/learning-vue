c24981702e3dc1a1339428c3430db548811dd3d1
verdantpath <m_dittmann@yahoo.com>
2019-12-04 18:54:07 -0800

    fix typo, eslint errors and test

--- shopping-list-single-file-components/shopping-list/src/vuex/mutations.js
+++ shopping-list-single-file-components/shopping-list/src/vuex/mutations.js
@@ -1,12 +1,12 @@
 import _ from 'underscore'
-import * as types from './mutations_types'
+import * as types from './mutation_types'
 
 function findById (state, id) {
-    return _.findWhere(state.shoppinglists, { id: id })
+  return _.findWhere(state.shoppinglists, { id: id })
 }
 
 export default {
-    [types.CHANGE_TITLE] (state, data) {
-        findById(state, data.id).title = data.title
-    }
+  [types.CHANGE_TITLE] (state, data) {
+    findById(state, data.id).title = data.title
+  }
 }
